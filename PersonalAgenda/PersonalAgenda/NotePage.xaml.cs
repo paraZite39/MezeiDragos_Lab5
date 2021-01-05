91bb914df218f97d7a092455f59154054465b9d1
paraZite <paraZite@DESKTOP-2U8TDJM>
2021-01-05 12:26:56 +0200

    Add project files.

---
+++ PersonalAgenda/PersonalAgenda/NotePage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PersonalAgenda.Models;
+
+namespace PersonalAgenda
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NotePage : ContentPage
+    {
+       async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Agenda)BindingContext;
+            note.Date = DateTime.UtcNow;
+            await App.Database.SaveNoteAsync(note);
+            await Navigation.PopAsync();
+        }
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Agenda)BindingContext;
+            await App.Database.DeleteNoteAsync(note);
+            await Navigation.PopAsync();
+        }
+        public NotePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
